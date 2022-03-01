5e1821548be97bc70c95ce20057eab1be81c21d3
Johan Strootman <johanstr@users.noreply.github.com>
2022-03-01 08:54:50 +0100

    Commentaar toegevoegd
    Door commentaar in de code toe te voegen kun je meer uitleg lezen.

--- js/bke.js
+++ js/bke.js
@@ -4,14 +4,22 @@
  *  Versie:     2022
  **************************************************/
 
-/*
+/*=====================================================================================
     CONSTANTEN
-*/
+    ----------
+    Hieronder plaatsen we variabelen waarvan we de waarde in ons programma niet zullen
+    veranderen. Constanten gebruiken we ook om met duidelijke variabele namen onze code
+    meer leesbaar te houden.
+=====================================================================================*/
 
 
-/*
+
+/*=====================================================================================
     VARIABELEN
-*/
+    ----------
+    De globale variabelen van ons programma. Ze zijn globaal gemaakt omdat we ze overal
+    in onze code willen kunnen gebruiken
+=====================================================================================*/
 let play_fields = document.querySelectorAll('.game-board img');
 let game_button = document.querySelector('.game-button');
 let element_timer = document.querySelector('.timer');
@@ -21,9 +29,15 @@ let element_score_player1 = document.querySelector('#player1-score');
 let element_score_player2 = document.querySelector('#player2-score');
 let element_round = document.querySelector('.round-info__number');
 
-/*
+
+
+/*====================================================================================
     INITIALISATIE
-*/
+    -------------
+    Initialiseren betekent zoveel als het uitvoeren van acties om je programma
+    klaar te maken voor gebruik. Sommige dingen moeten we eerst uitvoeren omdat
+    anders ons programma niet of niet goed gaat werken namelijk.
+====================================================================================*/
 window.onload = function () {
     game_button.addEventListener('click', buttonClickHandler);
     play_fields.forEach(cell => {
@@ -31,19 +45,57 @@ window.onload = function () {
     } )
 };
 
-/*
+
+/*===================================================================================
     MAIN FUNCTIONS
-*/
+    --------------
+    Main Functions zijn in ons geval en voor dit project de functies waarmee in feite
+    het spel gespeeld gaat worden.
+===================================================================================*/
+
+/**
+ * buttonClickHandler
+ * ------------------
+ * 
+ * De main function die op twee manieren kan reageren op het klikken op de button in 
+ * de game.
+ * 
+ * @param {object} event    event is een parameter van de functie die gevuld wordt
+ *                          door de browser met alle informatie omtrent het event.
+ */
 function buttonClickHandler(event)
 {
     console.log(event);
 }
 
 
+/**
+ * cellClickHandler
+ * ----------------
+ * 
+ * Dit is de functie die reageert op een klik op een cel op ons speelbord. In deze
+ * functie plaatsen we alle specifiek uit te voeren acties bij zo'n event.
+ * We hebben voor 9 cellen op ons speelbord echter slechts 1 functie. Deze functie
+ * moet dus zelf nog wel achterhalen op welke van de 9 cellen is geklikt.
+ * 
+ * @param {object} event    event is een parameter van de functie die gevuld wordt
+ *                          door de browser met alle informatie omtrent het event.
+ */
 function cellClickHandler(event)
 {
     console.log('Cell clicked');
 }
-/*
+
+
+
+/*==================================================================================
     HELPER FUNCTIONS
-*/
+    ----------------
+    Helper functions zijn functies die de main functions ondersteunen in het
+    uitvoeren van hun taak. Helper functions worden zo geprogrammeerd dat ze zich
+    altijd en uitsluitend op 1 taak richten. Maar door die taak in een helper
+    function te plaatsen hebben we de code voor die taak op 1 plaats staan en 
+    kunnen we deze code op meerdere plaatsen in ons programma hergebruiken.
+===================================================================================*/
+
+
